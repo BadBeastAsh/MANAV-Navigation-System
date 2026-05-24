32e4e1d9d4a43b0ed9f809abfed43ff76652cc90
Tanishq Garg <tanishqgarg1331@gmail.com>
2026-05-24 23:59:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Home from './pages/Home';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Aboutpage from './pages/Aboutpage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';    
+import { element } from 'three/webgpu';
+
+function App() {
+  return (
+    <div className="App  ">
+      {/* Content layers on top */}
+      <div className="content-container">
+        <Router>
+        <ResponsiveAppBar/>
+            <Routes>
+            <Route path='/' element={ <Home/> } ></Route>
+            <Route path='/about' element={<Aboutpage/>} ></Route>
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
